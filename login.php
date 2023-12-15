aea83902844de0051968ba83f597351ce135af02
moath3bd <moath3bd@gmail.com>
2023-12-15 23:59:41 +0300

    update

--- login.php
+++ login.php
@@ -45,10 +45,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="loginPageCss.css">
+    <link rel="stylesheet" href="assets/css/loginCss.css">
 </head>
 <body>
-<img class="wave" src="media/wave (2).png" alt="wave">
+<img class="wave" src="media/wave.png" alt="wave">
 	<div class="container">
 		<div class="img">
 			<img src="media/bg.png" alt="bg">
--- signup.php
+++ signup.php
@@ -54,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up Page</title>
-    <link rel="stylesheet" type="text/css" href="signupp.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/signupp.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
 </head>
