4b59a49d277d75317b2420d15476c637b8ca86ce
moath3bd <moath3bd@gmail.com>
2023-12-15 15:58:01 +0300

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce Micro</title>
+    <!--bootstrapcss link -->
+    <!--font awesome link -->
+</head>
+<body>
+    
+
+
+
+<!-- bootstrap js link -->
+</body>
+</html>
