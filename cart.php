506df2255d3df6ae0e96ace01a33f0fc19254d9d
moath3bd <moath3bd@gmail.com>
2023-12-17 11:20:31 +0300

    update

--- cart.php
+++ cart.php
@@ -17,7 +17,7 @@
             <div class="row">
 
               <div class="col-lg-7">
-                <h5 class="mb-3"><a href="#!" class="text-body"><i
+                <h5 class="mb-3"><a href="shop.php" class="text-body"><i
                       class="fas fa-long-arrow-alt-left me-2"></i>Continue shopping</a></h5>
                 <hr>
 
