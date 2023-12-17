06b20a725cb681847fe0d831675a66fbafacd930
moath3bd <moath3bd@gmail.com>
2023-12-17 08:57:13 +0300

    update

--- signup.php
+++ signup.php
@@ -51,6 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up Page</title>
